71feb34cb4811aa59fd81579f67b8394d46a1ace
Michal Muskala <michal@muskala.eu>
2016-11-12 21:20:23 +0100

    Support 1.3 only after all

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugAttack.Mixfile do
   def project do
     [app: :plug_attack,
      version: "0.1.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
