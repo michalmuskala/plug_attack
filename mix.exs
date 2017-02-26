448306b557f4fe111d8c9f87d56c0903d02b7baa
Michal Muskala <michal@muskala.eu>
2017-02-27 00:30:13 +0100

    Release v0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugAttack.Mixfile do
 
   def project do
     [app: :plug_attack,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
