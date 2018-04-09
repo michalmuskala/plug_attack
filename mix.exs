cbf24538c2b76b56f88ef634fc1f4b4c32ce7ec2
Michał Muskała <michal@muskala.eu>
2018-04-09 22:12:56 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugAttack.Mixfile do
 
   def project do
     [app: :plug_attack,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
