ca2947a311185ee4255e12cb94e8f9a6c9ca53ca
Michał Muskała <michal@muskala.eu>
2019-08-02 15:40:30 +0200

    Prepare 0.4.2 release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugAttack.Mixfile do
 
   def project do
     [app: :plug_attack,
-     version: "0.4.1",
+     version: "0.4.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
